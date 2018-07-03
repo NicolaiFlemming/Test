341bcfe8c8fbc293d7dcfa98eddda0bae611b099
N-Flemming <38251363+N-Flemming@users.noreply.github.com>
2018-07-03 19:11:07 +0200

    entfernen unnötiger bibliotheken und kommentierung
    Endlich auf deutsch

--- Mischencontroll 1-1 to 2-6/2scale_measure/scale_measure/scale_measure.ino
+++ Mischencontroll 1-1 to 2-6/2scale_measure/scale_measure/scale_measure.ino
@@ -1,51 +1,36 @@
-/*
- * circuits4you.com
- * 2016 November 25
- * Load Cell HX711 Module Interface with Arduino to measure weight in Kgs
- Arduino 
- pin 
- 2 -> HX711 CLK
- 3 -> DOUT
- 5V -> VCC
- GND -> GND
+
  
- Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
- The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
-*/
- 
-#include "HX711.h"  //You must have this library in your arduino library folder
+#include "HX711.h"
  
 #define DOUT  3
 #define CLK  2
  
 HX711 scale(DOUT, CLK);
  
-//Change this calibration factor as per your load cell once it is found you many need to vary it in thousands
-float calibration_factor = -96650; //-106600 worked for my 40Kg max scale setup 
+
+float calibration_factor = -92450; //Kalibrierungsfaktoir für unsere Load Cell aus 
  
 //=============================================================================================
-//                         SETUP
 //=============================================================================================
 void setup() {
   Serial.begin(9600);  
   Serial.println("Press T to tare");
-  scale.set_scale(-92450);  //Calibration Factor obtained from first sketch
-  scale.tare();             //Reset the scale to 0  
+  scale.set_scale(calibration_factor);  //Kalibrierungsfaktor aus scale_setup.ino
+  scale.tare();             //Tarieren der Waage  
 }
  
 //=============================================================================================
-//                         LOOP
 //=============================================================================================
 void loop() {
   Serial.print("Weight: ");
-  Serial.print((scale.get_units()*1000), 0);  //Up to 3 decimal points
-  Serial.println(" g"); //Change this to kg and re-adjust the calibration factor if you follow lbs
+  Serial.print((scale.get_units()*1000), 0);  //Bis zu 3 Dezimal stellen 
+  Serial.println(" g");
  
   if(Serial.available())
   {
     char temp = Serial.read();
     if(temp == 't' || temp == 'T')
-      scale.tare();  //Reset the scale to zero      
+      scale.tare();  //bei eingabe von 't' oder 'T' auf dem seriellen Monitor wird die waage tariert      
   }
 }
 //=============================================================================================

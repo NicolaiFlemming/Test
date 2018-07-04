5f46b574f869a53de566f31318cc7e85d7b639b0
N-Flemming <38251363+N-Flemming@users.noreply.github.com>
2018-07-04 13:07:19 +0200

    mischenconttroll 1-0

--- Mischencontroll 1-1 to 2-6/mischencontroll1-0/Mischencontroll1-0.ino
+++ Mischencontroll 1-1 to 2-6/mischencontroll1-0/Mischencontroll1-0.ino
@@ -1,92 +1,35 @@
 #include "HX711.h"
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-//benoetigte Bibliotheken
-
+ 
 #define DOUT  3
 #define CLK  2
-//fuer HX711 Inputs
-
+ 
 HX711 scale(DOUT, CLK);
+ 
 
-float calibration_factor = -92450; //Kalbirationsfaktor fuer die Waage
-
-int RelaisAlc = 4;  //Definieren des Ports fuer Relais 1
-int RelaisMix = 5;  //Definieren des Ports fuer Relais 2
-int VolAlc = 50-20; //50ml als experimentalwert. wird später durch eingabewert ersetzt. 20 ml Nachlauf nachdem Pumpe ausgestellt wurde
-int VolMix = 70-20; //70ml als experimentalwert. wird später durch eingabewert ersetzt. 20 ml Nachlauf nachdem Pumpe ausgestellt wurde  
-
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-//LCD-Verbindungen
-
-float weight;   //erklären der Variable, welche die Waage nutzt
-int weightP;  //Nachkommastellen loswerden, durch int
-
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------------------------------------------------------------
-
-
+float calibration_factor = -92450; //Kalibrierungsfaktoir für unsere Load Cell aus 
+ 
+//=============================================================================================
+//=============================================================================================
 void setup() {
-  
   Serial.begin(9600);  
-  Serial.println("T druecken, um zu tarieren");
-  scale.set_scale(calibration_factor); //kalibriert 2018-05-03
-  scale.tare(); //erste Kalibrierung
-
-  lcd.begin(16,2); //Ausgabe LCD-Bildschirm
-  lcd.backlight();
-  lcd.setCursor(0,0);  //Definieren wo auf LCD-Bildschirm Geschrieben wird
-  lcd.print("Gewicht:");  
-
-  delay(2000); //benoetigte Verzoegerung fuer tarieren
-  scale.tare();  //2. Kalibrierung. scheint aus irgendeinem <Grund besser zu funktionieren wenn man es anstellt 
-
-  digitalWrite(RelaisAlc, LOW);  
-  digitalWrite(RelaisMix, LOW);  //Ausstellen der Relais, eventl. an anfang der setup sektion setzen ??
+  Serial.println("Press T to tare");
+  scale.set_scale(calibration_factor);  //Kalibrierungsfaktor aus scale_setup.ino
+  scale.tare();             //Tarieren der Waage  
 }
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-void loop() 
-{
-  weight = (scale.get_units()*1000); //Umwandlung in Gramm
-  weightP = weight;  //definieren der variable fuer Gewicht 
-  lcd.setCursor(0,1);
-  lcd.print(weightP); //Ausgabe des Gewichts auf dem LCD Bildschirm
-  lcd.print(" g  "); //Einheit
-  
-   if( VolAlc < weightP ) //Schleife fuer Relais 1 auf Pin4
-    {
-    digitalWrite(RelaisAlc, HIGH);
-    }
-  else if(RelaisAlc==HIGH)
-    {
-    digitalWrite(RelaisAlc, LOW); 
-    }
-
-  if( VolMix + (VolAlc+20) < weightP) //Schleife fuer Relais 2 Auf Pin5
-  {
-    digitalWrite(RelaisMix, HIGH);
-  }
-  else if(RelaisMix==HIGH)
-  {
-   digitalWrite(RelaisMix, LOW); 
-  }
-
- //beide Schleifen zusammen funktionieren noch nicht zusammen. einzeln haben sie beide schon funktioniert. wo ist der Denkfehler? Neue Schleife aufbauen ? 
- //While schleifen funktionieren hier nicht weil sie erst komplett abgearbeitet werden und man dann keine Kontinuierlichen Werte von der Waage zurück bekommt.
- //Kann man 2 loop funktionen (Void Loop () ) gleichzeitig laufen lassen? Eine fuer die waage und die andere fuer die Relais?  
-   
-   if(Serial.available()) //Bei Input von T -> tarieren
+ 
+//=============================================================================================
+//=============================================================================================
+void loop() {
+  Serial.print("Weight: ");
+  Serial.print((scale.get_units()*1000), 0);  //Bis zu 3 Dezimal stellen 
+  Serial.println(" g");
+ 
+  if(Serial.available())
   {
     char temp = Serial.read();
     if(temp == 't' || temp == 'T')
-      scale.tare();  //Befehl zum Tarieren wenn der void loop schon begonnen hat
-   }
-   
-  delay(10); //Verzoegerung  //Verzoegerung des gesamten Loops
+      scale.tare();  //bei eingabe von 't' oder 'T' auf dem seriellen Monitor wird die waage tariert      
+  }
 }
+//=============================================================================================
+

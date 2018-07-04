08774ca06938c1106a211f33e22a1271650c6066
baedecker <t.baedecker@tu-bs.de>
2018-07-04 17:02:03 +0200

    benutzer 1 und 2 fertig

--- benutzer/benutzer2.cpp
+++ benutzer/benutzer2.cpp
@@ -1,15 +1,23 @@
-#include <iostream>					// Praeprozessoranweisung
+/*-------------------------------------------Beschreibung-------------------------------------------------
+Dies ist die fertige Testversion für die Benutzerklassen. Der Code in der Hauptfunktion wird in stark 
+abgewandelter Form in der loop-Funktion der ino-Datei eingefügt werden. Alle Befehle sind zur einfachen
+Überprüfung für das Terminal (io stream) geschrieben und müssen durch für den Arduino interpretierbare 
+Anweisungen umgeschrieben werden. 
+---------------------------------------------------------------------------------------------------------*/
 
+#include <iostream>					
 using namespace std;
+
 //Klassendefinition
 class benutzer
 {
     private:
-    // Attribute   
-    int kge;
-    char gen;        
-    float sufflvl;
-    int alkges=12; // Insgesamt verzehrter Alkohol zu Demo_Zwecken hier auf einen wahrlosen Wert gesetzt.
+    // Attribute  
+
+    int kge;          // Körpergewicht
+    char gen;         // Geschlecht  
+    float sufflvl;    // Promillegehalt im Blut
+    int alkges=12;    // Insgesamt verzehrter Alkohol zu Demo Zwecken hier auf einen wahrlosen Wert gesetzt.
     
     public:
     // Methoden               
@@ -25,25 +33,26 @@ class benutzer
 };
 
 int main(){ 
-int auswahl;
+int auswahl;          // Benutzerauswahl
+// Erstellen der Objekte
 benutzer benutzer1;
 benutzer benutzer2;
 benutzer benutzer3;
 benutzer benutzer4;
 benutzer benutzer5;
 benutzer benutzer6;
+// Auswahl des Benutzers
 cout<<"druecken sie eine Zahl von 1-6 um einen benutzer auszuwaehlen"<<endl;
 cin>>auswahl;
 switch (auswahl){
     case 1:{
         
-        int gew;
-        char gesch;      // provisorische Variable für gender zur übergabe an methode
-       
+       int gew;                // Provisorische Variable für Gewicht zur Übergabe an Methode
+       char gesch;            // Provisorische Variable für gender zur Übergabe an Methode
         
         cout<<"Koerpergewicht eingeben"<<endl;
         cin>>gew;
-        cout<<"bitte Geschlecht angeben [M] || [W]"<<endl;
+        cout<<"Bitte Geschlecht angeben [M] || [W]"<<endl;
         cin>> gesch;
         
        
@@ -51,13 +60,16 @@ switch (auswahl){
         benutzer1.set_kge(gew);
         benutzer1.set_gen(gesch);
         benutzer1.err_sufflvl(gew, gesch, benutzer1.get_alkges());
-         if (benutzer1.err_sufflvl(gew,gesch,benutzer1.get_alkges())== false)
+        
+          while ( benutzer1.err_sufflvl(gew,gesch,benutzer1.get_alkges())== false)
          {
             cout<<"Fehler! bitte M oder W eingeben"<<endl;
             cin>>gesch;
             benutzer1.set_gen(gesch);
             benutzer1.err_sufflvl(gew, gesch, benutzer1.get_alkges());
          } 
+        
+
             cout<<"      Benutzer1:"<<endl;
             cout<<"Gewicht    =    "<<benutzer1.get_kge()<<endl;
             cout<<"Geschlecht =    "<<benutzer1.get_gen()<<endl;
@@ -67,8 +79,8 @@ switch (auswahl){
     }
      case 2:{
         
-        int gew;
-        char gesch;      // provisorische Variable für gender zur übergabe an methode
+       int gew;                // Provisorische Variable für Gewicht zur Übergabe an Methode
+       char gesch;            // Provisorische Variable für gender zur Übergabe an Methode
        
         
         cout<<"Koerpergewicht eingeben"<<endl;
@@ -82,13 +94,16 @@ switch (auswahl){
         benutzer2.set_gen(gesch);
         benutzer2.err_sufflvl(gew, gesch, benutzer2.get_alkges());
 
-        if (benutzer2.err_sufflvl(gew,gesch,benutzer2.get_alkges())== false)
+        
+        while (benutzer2.err_sufflvl(gew,gesch,benutzer2.get_alkges())== false)
          {
             cout<<"Fehler! bitte M oder W eingeben"<<endl;
             cin>>gesch;
             benutzer2.set_gen(gesch);
             benutzer2.err_sufflvl(gew, gesch, benutzer2.get_alkges());
          } 
+         
+
         cout<<"      Benutzer2:"<<endl;
         cout<<"Gewicht    =    "<<benutzer2.get_kge()<<endl;
         cout<<"Geschlecht =    "<<benutzer2.get_gen()<<endl;
@@ -113,41 +128,36 @@ switch (auswahl){
 
 //Methoden 
 
-int benutzer::get_kge() {
+int benutzer::get_kge() {        // Methode zum zugriff auf Körpergewicht
  return kge;
 }
 
-
-char benutzer::get_gen(){
+char benutzer::get_gen(){       // Methode zum Zugriff auf Geschlecht
  return gen;
 }
-float benutzer::get_sufflvl(){
+float benutzer::get_sufflvl(){  // Methode zum zugriff auf Promille-Gehalt
     
-    return sufflvl; 
-   
+    return sufflvl;   
 }
-int benutzer::get_alkges(){
+int benutzer::get_alkges(){     // Methode zum zugriff auf insgesamt verzehrten Alkohol
     return alkges;
 }
 
-
-
-void  benutzer::set_kge(int kge){
+void  benutzer::set_kge(int kge){  // Methode zum festlegen des Körpergewichts
     this->kge =kge;
 }
-void benutzer::set_gen(char gen){
+void benutzer::set_gen(char gen){  // Methode zum festlegen des Geschlechts
     this->gen=gen;
 }
-bool benutzer::err_sufflvl(int kge, char gen, int alkges ){
+bool benutzer::err_sufflvl(int kge, char gen, int alkges ){  // Errechnen des Promille-Gehalts
     
-    
-    if (this->gen=='m' || this->gen=='M')
-    {
+    //Das Errechen des Alkoholgehalts im Blut erforlgt bei Männern und Frauen unterschiedlich, da der Wasser und Fett gehalt des Körpers variiert.
+   
+    if (this->gen=='m' || this->gen=='M'){
         this-> sufflvl = this->alkges/(this->kge*0.68);
         return true;
     }
-    if (this->gen=='W' || this->gen=='w')
-    {
+    if (this->gen=='W' || this->gen=='w'){
         this->sufflvl = this->alkges/(this->kge*0.55);
         return true;
     }
@@ -156,9 +166,9 @@ bool benutzer::err_sufflvl(int kge, char gen, int alkges ){
         return false;
     }
 
+    // Bei falscheingabe des Geschlechts wird das Wert der Methode auf false gesetzt. Dies wird später in der Hauptfunktion abgefragt
 }
 
 
 
-//anzal gramm durch mei männern kgwe *0,68
-//bei frauen*0,55
+
